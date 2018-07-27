5e26c4e7b8d871c721a6ea0d06be8fc44413c434
Jordan De Mello <31478340+jordandemello@users.noreply.github.com>
2018-07-26 21:23:43 -0700

    a

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const expressValidator = require('express-validator');
 const mongoose = require('mongoose');
-const Signature = require('./models/signature.js')
+const UserModel = require('./models/signature.js')
 const app = express();
 const url = 'mongodb://jordan:admin1234@ds253871.mlab.com:53871/erp-hackathon';
 
@@ -13,20 +13,71 @@ app.get('/', function(req, res) {
   res.json('you did it');
 });
 //==========================//
-//====GET ALL SIGNATURES===//
-app.get('/api/signatures', function(req, res) {
-  Signature.find({}).then(eachOne => {
+//====GET ALL USERS===//
+app.get('/api/users', function(req, res) {
+  UserModel.find({}).then(eachOne => {
     res.json(eachOne);
-    })
   })
+})
 //==========================//
-//====POST NEW SIGNATURE===//
-app.post('/api/signatures', function(req, res) {
-  Signature.create({
-    guestSignature: req.body.SignatureOfGuest,
-    message: req.body.MessageofGuest,
-  }).then(signature => {
-    res.json(signature)
+
+//====GET ALL GROUPS===//
+app.get('/api/groups', function(req, res) {
+  UserModel.find({}, 'groupName').then(eachOne => {
+    res.json(eachOne);
+  })
+})
+//==========================//
+
+//====GET ALL USERS FOR A GROUP===//
+app.get('/api/groupUsers', function(req, res) {
+  UserModel.find({groupName : req.body.groupName}).then(eachOne => {
+    res.json(eachOne);
+  })
+})
+//==========================//
+
+//====POST NEW USER===//
+app.post('/api/users', function(req, res) {
+  UserModel.create({
+    firstName: req.body.FirstName,
+    lastName: req.body.LastName,
+    isCorporate: req.body.IsCorporate,
+    groupName: req.body.GroupName,
+    groupPosition: req.body.GroupPosition,
+    totalDistance: req.body.TotalDistance,
+  }).then(user => {
+    res.json(user)
+  });
+});
+//==========================//
+
+//====UPDATE DISTANCE FOR USER===//
+app.post('/api/users', function(req, res) {
+  UserModel.create({
+    firstName: req.body.FirstName,
+    lastName: req.body.LastName,
+    isCorporate: req.body.IsCorporate,
+    groupName: req.body.GroupName,
+    groupPosition: req.body.GroupPosition,
+    totalDistance: req.body.TotalDistance,
+  }).then(user => {
+    res.json(user)
+  });
+});
+//==========================//
+
+//====DELETE USER===//
+app.post('/api/users', function(req, res) {
+  UserModel.create({
+    firstName: req.body.FirstName,
+    lastName: req.body.LastName,
+    isCorporate: req.body.IsCorporate,
+    groupName: req.body.GroupName,
+    groupPosition: req.body.GroupPosition,
+    totalDistance: req.body.TotalDistance,
+  }).then(user => {
+    res.json(user)
   });
 });
 //==========================//
@@ -40,6 +91,3 @@ mongoose.connect(url, function (err, db) {
  }
 });
 //==========================//
-
-app.listen(27017);
-console.log('starting application.  Good job!');
--- signature.js
+++ signature.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
-const signatureSchema = new Schema({
-  guestSignature: {
-    type: mongoose.Schema.Types.Mixed,
+const userSchema = new Schema({
+  firstName: {
+    type: String,
     required: true,
   },
-  message: {
-    type: mongoose.Schema.Types.Mixed,
+  lastName: {
+    type: String,
     required: true,
   },
+  isCorporate: {
+    type: Boolean,
+    required: true,
+  },
+  groupName: {
+    type: String,
+    required: true,
+  },
+  groupPosition: {
+    type: String,
+    required: true,
+  },
+  totalDistance: {
+    type: mongoose.Schema.Types.Decimal128,
+    required: true,
+  }
 })
-const Signature = mongoose.model('Signature', signatureSchema);
-module.exports = Signature;
+const UserModel = mongoose.model('UserModel', userSchema);
+module.exports = UserModel;
